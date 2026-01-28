8e313902daa02c7d7b100ab50c313123b0ca2d6d
Dmitrii Ozornin <ozornindimitry@uandex.ru>
2026-01-28 12:35:24 +0300

    first commit

---
+++ my-app/app/about/page.tsx
@@ -0,0 +1,13 @@
+export default function About() {
+  return (
+    <section>
+      <h1>Обо мне</h1>
+      <h2>Дмитрий Озорнин</h2>
+      <ul>
+        <li>Мне 32 года</li>
+        <li>Люблю собак</li>
+        <li>Хочу расти в it</li>
+      </ul>
+    </section>
+  );
+}
